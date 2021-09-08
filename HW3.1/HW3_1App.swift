7b9660d3a9c01932ddcab10430dae36405f84e8b
Dorokhov Alexander <svdfold@gmail.com>
2021-09-08 17:31:24 +0300

    Initial Commit

---
+++ HW3.1/HW3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_1App.swift
+//  HW3.1
+//
+//  Created by Mike Albright on 08.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HW3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
